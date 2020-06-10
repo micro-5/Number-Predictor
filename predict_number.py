8a955a2e2ca9553d46907267b7310d9030adb826
micro-5 <64730893+micro-5@users.noreply.github.com>
2020-06-10 18:44:00 +0530

    Update predict_number.py

--- predict_number.py
+++ predict_number.py
@@ -1,37 +1,32 @@
 import tensorflow as tf
-mnist = tf.keras.datasets.mnist
-
-(x_train, y_train),(x_test, y_test) = mnist.load_data()
-
-import matplotlib.pyplot as plt
-plt.imshow(x_train[0])
-print(y_train[0])
-print(x_train[0])
-
-x_train = x_train/255.0
-x_test = x_test/255.0
 
 class myCallBack(tf.keras.callbacks.Callback):
   def on_epoch_end(self, epoch, log={}):
-    if(log.get('accuracy') > 0.99):
-      print("\nReached 99% accuracy so cancelling training!")
+    if(log.get('accuracy') > 0.998):
+      print("\nReached 99.8% accuracy so cancelling training!")
       self.model.stop_training = True
 
 callbacks = myCallBack()
 
 
+mnist = tf.keras.datasets.mnist
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+
+training_images = training_images.reshape(60000, 28, 28, 1)
+training_images = training_images/255.0
+
+test_images = test_images.reshape(10000, 28, 28, 1)
+test_images = test_images/255.0
+
 model = tf.keras.models.Sequential([
-
-tf.keras.layers.Flatten(),
-tf.keras.layers.Dense(128, activation = tf.nn.relu),
-tf.keras.layers.Dense(10, activation = tf.nn.softmax)
-
+            tf.keras.layers.Conv2D(32, (3,3), activation = 'relu', input_shape = (28,28,1)),
+            tf.keras.layers.MaxPooling2D(2,2),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(128, activation = 'relu'),
+            tf.keras.layers.Dense(10, activation = 'softmax')
 ])
 
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-
-
-model.fit(x_train, y_train, epochs = 10, callbacks=[callbacks])
-model.evaluate(x_test, y_test)
+model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+model.fit(training_images, training_labels, epochs=10, callbacks=[callbacks])
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print("Test Accuracy = ", test_acc)
